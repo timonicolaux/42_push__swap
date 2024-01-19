0b3517813bce7983b920db31977796353cfda377
Timo Nicolaux <tnicolau@z3r13p1.42lyon.fr>
2024-01-19 13:26:57 +0100

    update to latest version

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tnicolau <tnicolau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 11:26:21 by tnicolau          #+#    #+#             */
-/*   Updated: 2024/01/16 13:32:32 by tnicolau         ###   ########.fr       */
+/*   Updated: 2024/01/19 09:16:15 by tnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,22 @@ int	push_swap(char **str, int i)
 {
 	t_list	*a_lst;
 	t_list	*b_lst;
+	t_list	*new_lst;
 
 	a_lst = NULL;
 	b_lst = NULL;
+	new_lst = NULL;
 	if (error_check(str, i))
 		return (1);
 	while (str[i])
 	{
-		ft_lstadd_back(&a_lst, ft_lstnew(ft_atoi(str[i])));
+		new_lst = ft_lstnew(ft_atoi(str[i]));
+		if (!new_lst)
+			break ;
+		ft_lstadd_back(&a_lst, new_lst);
 		i++;
 	}
-	if (lst_length(&a_lst) == 1)
-		ft_printf("");
-	else if (is_sorted(&a_lst))
-		ft_printf("");
-	else
+	if (lst_length(&a_lst) > 1 && !is_sorted(&a_lst) && new_lst)
 		algo(&a_lst, &b_lst);
 	clear_lst(&a_lst);
 	clear_lst(&b_lst);
@@ -102,22 +103,22 @@ int	main(int ac, char **av)
 	int		check_error;
 
 	if (ac < 2)
-		ft_printf("");
+		return (0);
 	else if (ac == 2)
 	{
 		numbers = ft_split(av[1], ' ');
 		check_error = push_swap(numbers, 0);
 		if (check_error)
-			ft_printf("Error\n");
+			write(2, "Error\n", 6);
 		free_split(numbers);
 	}
 	else if (ac > 2)
 	{
 		check_error = push_swap(av, 1);
 		if (check_error)
-			ft_printf("Error\n");
+			write(2, "Error\n", 6);
 	}
 	else
-		ft_printf("Error\n");
+		write(2, "Error\n", 6);
 	return (0);
 }
